a529dacbddc130ed63813cb1e04b34cdf70c518f
Ranwala, Ravindra <Ravindra.Ranwala@pearson.com>
2017-04-04 07:50:15 +0530

    weather forecasting app was implemented using redux as the data management library.

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'd4c7fdc5ea5f921eb81842aa1a0cd481';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
